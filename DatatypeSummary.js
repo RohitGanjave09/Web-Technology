8b438dad2c17cac5824fc237932841a7c24705fa
Rohit <rohitganjave87@gamil.com>
2026-01-21 07:58:37 +0530

    Javascript

---
+++ DatatypeSummary.js
@@ -0,0 +1,27 @@
+// symbol eg symbol is mostly used in react
+//it used for unique ids 
+
+const anotherId=Symbol("1,2,3");
+console.log(anotherId);
+console.log(typeof(anotherId));
+
+const Id=123;
+console.log(Id===anotherId);
+
+//Array
+
+const Heros=["king","don","ak"];
+const nums=[1,2,3];
+console.log(Heros);
+console.log(nums);
+
+//Objects key and value pairs
+const obj={
+    fName:"js",
+    age:21
+}
+console.log(obj.age);
+
+
+
+
---
+++ String.js
@@ -0,0 +1,16 @@
+let str ="rohit"
+console.log(str,typeof str);
+
+//String intepolation
+
+console.log(`hello my name is ${str}`);
+
+let fName="rohit";
+console.log(`${fName}`);
+const gameCount=new String("Ak");
+
+console.log(gameCount.length);
+console.log(gameCount.toLowerCase());
+console,log(gameCount.toUpperCase());
+console.log(gameCount.charAt());//2 num chya index la return kartay 
+console.log(gameCount.indexOf())//give the index of giving letter
